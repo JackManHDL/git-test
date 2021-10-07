49b8e09319dcbb1554d10b4b90cbf038eeb19180
xie <1710725748@qq.com>
2021-10-07 15:06:19 +0800

    ddfdfg

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,10 +1,6 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
-        System.out.println("hot-fix 冲突合并到master");
-        System.out.println("ok");
-        System.out.println("hot-fix 正常合并到master");
-        System.out.println("正常合并过来");
-        System.out.println("ssss");
+
     }
 }
